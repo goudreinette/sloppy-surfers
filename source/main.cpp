2b7491db1f8cc0ad9e6dff84c67715bcc2018d55
Rein van der Woerd <reinvanderwoerd@me.com>
2025-01-07 20:54:07 +0100

    dualscreen teapot test working!

--- source/main.cpp
+++ source/main.cpp
@@ -97,88 +97,90 @@ void load_mp3_material() {
 
 
 int main(int argc, char *argv[]) {
-    nitroFSInit(NULL);
-    NF_SetRootFolder("NITROFS");
+    test_dual_screen::main();
 
-    mic::setup();
+//     nitroFSInit(NULL);
+//     NF_SetRootFolder("NITROFS");
 
-    // setup scene
-    scene_data scene = {
-        .camera = NE_CameraCreate()
-    };
+//     mic::setup();
 
-
-    irqEnable(IRQ_HBLANK);
-    irqSet(IRQ_VBLANK, NE_VBLFunc);
-    irqSet(IRQ_HBLANK, NE_HBLFunc);
-
-    // Init Nitro Engine in normal 3D mode
-    NE_Init3D();
-
-    // libnds uses VRAM_C for the text console, reserve A and B only
-    NE_TextureSystemReset(0, 0, NE_VRAM_AB);
-
-    // Init console in non-3D screen
-    // Setup models and scenes
-    load_mp3_material();
-
-    hearts_and_comments::load_assets(&scene);
-    wout_bust::load_assets(&scene);
-    trein::load_assets(&scene);
+//     // setup scene
+//     scene_data scene = {
+//         .camera = NE_CameraCreate()
+//     };
 
 
-    // Set coordinates for the camera
-    NE_CameraSet(scene.camera,
-                 -10, 0, 0,  // Position
-                  0, 0, 0,  // Look at
-                  0, 1, 0); // Up direction
+//     irqEnable(IRQ_HBLANK);
+//     irqSet(IRQ_VBLANK, NE_VBLFunc);
+//     irqSet(IRQ_HBLANK, NE_HBLFunc);
+
+//     // Init Nitro Engine in normal 3D mode
+//     NE_Init3D();
+
+//     // libnds uses VRAM_C for the text console, reserve A and B only
+//     NE_TextureSystemReset(0, 0, NE_VRAM_AB);
+
+//     // Init console in non-3D screen
+//     // Setup models and scenes
+//     load_mp3_material();
+
+//     hearts_and_comments::load_assets(&scene);
+//     wout_bust::load_assets(&scene);
+//     trein::load_assets(&scene);
+
+
+//     // Set coordinates for the camera
+//     NE_CameraSet(scene.camera,
+//                  -10, 0, 0,  // Position
+//                   0, 0, 0,  // Look at
+//                   0, 1, 0); // Up direction
 
   
 
-    // We set up a light and its color
-    NE_LightSet(0, NE_White, -0.5, -0.5, -0.5);
-//    setBackdropColorSub(RGB8(95,205,228)); // light
+//     // We set up a light and its color
+//     NE_LightSet(0, NE_White, -0.5, -0.5, -0.5);
+// //    setBackdropColorSub(RGB8(95,205,228)); // light
 
 
-    consoleDemoInit();
-    // Wrap values of parameters
+//     consoleDemoInit();
+//     // Wrap values of parameters
 
-    // Enable/update fog
-//    NE_FogEnable(2, RGB8(95,205,228), 31, 4, 0x7C00);
-    NE_FogEnable(2, RGB8(0,0,0), 31, 5, 0x7C00);
+//     // Enable/update fog
+// //    NE_FogEnable(2, RGB8(95,205,228), 31, 4, 0x7C00);
+//     NE_FogEnable(2, RGB8(0,0,0), 31, 5, 0x7C00);
 
-//    NE_SpecialEffectNoiseConfig(31);
-    // NE_SpecialEffectSet(NE_NOISE);
-
-
-
-    // Load sloppy text
-    scene.sloppy_text = NE_ModelCreate(NE_Static);
-
-    text_material = NE_MaterialCreate();
-    // Load mesh from RAM and assign it to the object "Model".
-    NE_ModelLoadStaticMesh(scene.sloppy_text, sloppy_bin);
-    // Assign texture to model...
-    NE_ModelSetMaterial(scene.sloppy_text, text_material);
-    // Set some properties to the material
-    NE_MaterialSetProperties(text_material,
-                             RGB15(31, 31, 31), // Diffuse
-                             RGB15(15, 15, 15), // Ambient
-                             RGB15(31, 31, 31),    // Specular
-                             RGB15(0, 0, 0),    // Emission
-                             false, true);     // Vertex color, use shininess table
+// //    NE_SpecialEffectNoiseConfig(31);
+//     // NE_SpecialEffectSet(NE_NOISE);
 
 
-    while (true)
-    {
-        t++;
 
-        // Wait for next frame
-        NE_WaitForVBL(NE_UpdateFlags::NE_UPDATE_PHYSICS);
+//     // Load sloppy text
+//     scene.sloppy_text = NE_ModelCreate(NE_Static);
+
+//     text_material = NE_MaterialCreate();
+//     // Load mesh from RAM and assign it to the object "Model".
+//     NE_ModelLoadStaticMesh(scene.sloppy_text, sloppy_bin);
+//     // Assign texture to model...
+//     NE_ModelSetMaterial(scene.sloppy_text, text_material);
+//     // Set some properties to the material
+//     NE_MaterialSetProperties(text_material,
+//                              RGB15(31, 31, 31), // Diffuse
+//                              RGB15(15, 15, 15), // Ambient
+//                              RGB15(31, 31, 31),    // Specular
+//                              RGB15(0, 0, 0),    // Emission
+//                              false, true);     // Vertex color, use shininess table
+
+
+    // while (true)
+    // {
+    //     t++;
+
+    //     // Wait for next frame
+    //     NE_WaitForVBL(NE_UpdateFlags::NE_UPDATE_PHYSICS);
             
-        // Draw scene
-        NE_ProcessArg(draw_3d_scene, &scene);
-    }
+    //     // Draw scene
+    //     NE_ProcessArg(draw_3d_scene, &scene);
+    // }
 
     return 0;
 }
--- source/test_dual_sceen.hpp
+++ source/test_dual_sceen.hpp
@@ -1,6 +1,174 @@
 #pragma once
 
+#include <NEMain.h>
+
+#include "teapot_bin.h"
+#include "sphere_bin.h"
+
 
 namespace test_dual_screen {
+    
+    typedef struct {
+        NE_Camera *CameraTop, *CameraBottom;
+        NE_Model *Teapot, *Sphere;
+    } SceneData;
+
+    
+
+    void Draw3DSceneTop(void *arg)
+    {
+        SceneData *Scene = (SceneData*) arg;
+
+        NE_ClearColorSet(NE_Green, 31, 63);
+
+        NE_CameraUse(Scene->CameraTop);
+        NE_ModelDraw(Scene->Teapot);
+    }
+
+    void Draw3DSceneBottom(void *arg)
+    {
+        SceneData *Scene = (SceneData*) arg;
+
+        NE_ClearColorSet(NE_Red, 31, 63);
+
+        NE_CameraUse(Scene->CameraBottom);
+        NE_ModelDraw(Scene->Teapot);
+    }
+
+    void init_all(SceneData *Scene)
+    {
+        // Allocate objects...
+        Scene->Teapot = NE_ModelCreate(NE_Static);
+        Scene->Sphere = NE_ModelCreate(NE_Static);
+        Scene->CameraTop = NE_CameraCreate();
+        Scene->CameraBottom = NE_CameraCreate();
+
+        // Setup camera
+        NE_CameraSet(Scene->CameraTop,
+                    0, 0, -2,
+                    0, 0, 0,
+                    0, 1, 0);
+        
+        NE_CameraSet(Scene->CameraBottom,
+                    0, 2.5, -2,
+                    0, 2.5, 0,
+                    0, 1, 0);
+
+        // Load models
+        NE_ModelLoadStaticMesh(Scene->Teapot, teapot_bin);
+        NE_ModelLoadStaticMesh(Scene->Sphere, sphere_bin);
+
+        // Set light color and direction
+        NE_LightSet(0, NE_White, -0.5, -0.5, -0.5);
+    }
+
+    int main()
+    {
+        SceneData Scene = { 0 };
+
+        // This is needed for special screen effects
+        irqEnable(IRQ_HBLANK);
+        irqSet(IRQ_VBLANK, NE_VBLFunc);
+        irqSet(IRQ_HBLANK, NE_HBLFunc);
+
+        // Init dual 3D mode and console
+        NE_InitDual3D();
+        NE_InitConsole();
+
+        init_all(&Scene);
+
+        bool console = true;
+
+        while (1)
+        {
+            // NE_WaitForVBL();
+
+            // Draw 3D scenes
+            NE_ProcessDualArg(Draw3DSceneBottom, Draw3DSceneTop, &Scene, &Scene);
+
+            // Refresh keys
+            scanKeys();
+            uint32_t keys = keysHeld();
+            uint32_t kdown = keysDown();
+
+            if (console)
+            {
+                printf("\x1b[0;0H"
+                    "START: Lock CPU until released\n"
+                    "A: Sine effect.\n"
+                    "B: Deactivate effect.\n"
+                    "SELECT: Dual 3D DMA mode\n"
+                    "X: Dual 3D FB mode (no console)\n"
+                    "Y: Dual 3D mode\n"
+                    "Pad: Rotate.\n");
+            }
+
+            // Lock CPU in an infinite loop to simulate a drop in framerate
+            while (keys & KEY_START)
+            {
+                scanKeys();
+                keys = keysHeld();
+            }
+
+            // Rotate model
+            if (keys & KEY_UP)
+            {
+                // NE_ModelRotate(Scene.Sphere, 0, 0, 2);
+                // NE_ModelRotate(Scene.Teapot, 0, 0, 2);
+                NE_ModelTranslate(Scene.Sphere, 0, 0.5, 0);
+                NE_ModelTranslate(Scene.Teapot, 0, 0.5, 0);
+            }
+            if (keys & KEY_DOWN)
+            {
+                // NE_ModelRotate(Scene.Sphere, 0, 0, -2);
+                // NE_ModelRotate(Scene.Teapot, 0, 0, -2);
+                NE_ModelTranslate(Scene.Sphere, 0, -.5, 0);
+                NE_ModelTranslate(Scene.Teapot, 0, -.5, 0);
+            }
+            if (keys & KEY_RIGHT)
+            {
+                NE_ModelRotate(Scene.Sphere, 0, 2, 0);
+                NE_ModelRotate(Scene.Teapot, 0, 2, 0);
+            }
+            if (keys & KEY_LEFT)
+            {
+                NE_ModelRotate(Scene.Sphere, 0, -2, 0);
+                NE_ModelRotate(Scene.Teapot, 0, -2, 0);
+            }
+
+            // Deactivate effect
+            if (kdown & KEY_B)
+                // NE_SpecialEffectSet(0);
+            // Activate effect
+            if (kdown & KEY_A)
+                NE_SpecialEffectSet(NE_SINE);
+
+            if (kdown & KEY_Y)
+            {
+                // NE_SpecialEffectSet(0);
+                NE_InitDual3D();
+                NE_InitConsole();
+                init_all(&Scene);
+                console = true;
+            }
+            if (kdown & KEY_X)
+            {
+                // NE_SpecialEffectSet(0);
+                NE_InitDual3D_FB();
+                init_all(&Scene);
+                console = false;
+            }
+            if (kdown & KEY_SELECT)
+            {
+                // NE_SpecialEffectSet(0);
+                NE_InitDual3D_DMA();
+                NE_InitConsole();
+                init_all(&Scene);
+                console = true;
+            }
+        }
+
+        return 0;
+    }
 
 }
